7bc4b538c3ec8ed91842ea37c0fdf2a9ebd5780a
TM_Brodie <brodie.balser@ticketmaster.com>
2024-11-05 08:13:50 -0500

    small changes to force push

--- src/git/forcePush.ts
+++ src/git/forcePush.ts
@@ -4,13 +4,11 @@ import { exec, echo } from 'shelljs'
 
 const fusrodah = async () => {
   echo(chalk.green.bold('FUS-RO-DAAAAAHHH!'))
-  const result = await exec('git push --bob')
+  const result = await exec('git push --force --no-verify')
+  const error = result.code !== 0
 
-  if (result.code !== 0) {
-    console.log(result)
-    echo(chalk.red.bold('you are not a truly dragon born.'))
-  } else {
-    echo(chalk.green.bold('FUS-RO-DAAAAAHHH!'))
+  if (error) {
+    echo(chalk.red.italic('you are not a true dragon born.'))
   }
 }
 
