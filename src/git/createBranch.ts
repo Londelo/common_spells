de9313e572b9ea2ec3da35539781dc4d22ea71fc
TM_Brodie <brodie.balser@ticketmaster.com>
2024-11-05 09:48:40 -0500

    MAIN: wip createBranch

--- src/git/createBranch.ts
+++ src/git/createBranch.ts
@@ -1,3 +1,20 @@
+#!/usr/bin/env node
+import chalk from 'chalk'
+import { exec, echo, config } from 'shelljs'
+
+const createBranch = async () => {
+  config.silent = true
+  config.fatal = true
+
+  const defaultBranch = exec("git remote show origin | grep 'HEAD branch' | awk '{print $NF}'").stdout
+    .replace('\n', '')
+
+  console.log(defaultBranch)
+}
+
+(async () => await createBranch())();
+
+
 //get default branch
 //git fetch and pull
 //git checkout -b branch name
