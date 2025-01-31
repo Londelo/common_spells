06ec8be7be8f419eb8e350f45520a0f6d86543a1
TM_Brodie <brodie.balser@ticketmaster.com>
2025-01-31 11:02:22 -0500

    MAIN: wip rebase

--- src/shared/gitLogOneLine.ts
+++ src/shared/gitLogOneLine.ts
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 import { exec, echo } from 'shelljs'
 import { yellow } from './colors'
+import inquirer from 'inquirer'
 
 const stylizeLogs = (logs: string) =>
   logs.split('\n')
@@ -11,10 +12,32 @@ const stylizeLogs = (logs: string) =>
   })
   .join('\n')
 
-const gitLogOneLine = async () => {
+const selectCommitTag = (answer: {log:string}) => answer.log.split(' ')[0]
+
+async function selectLog(logs: string) {
+  const logsArray = logs.split('\n')
+
+  const answers = await inquirer.prompt([
+    {
+      type: 'list',
+      name: 'log',
+      message: 'Choose your destination, my love.',
+      choices: logsArray
+    }
+  ]);
+
+  return selectCommitTag(answers)
+}
+
+const gitLogOneLine = async (select: boolean = false) => {
   const logCommand = 'git log --oneline \n'
   echo(yellow(logCommand))
   const logs = await exec(logCommand, { silent: true }).stdout
+
+  if(select) {
+    return await selectLog(logs)
+  }
+
   echo(stylizeLogs(logs))
 }
 
--- src/spells/git/rebaseCommits.ts
+++ src/spells/git/rebaseCommits.ts
@@ -1,66 +1,18 @@
 #!/usr/bin/env node
 import { exec, echo, exit, config } from 'shelljs'
-import inquirer from 'inquirer'
 import errorHandlerWrapper from '../../shared/errorHandlerWrapper';
-import { selectCurrentBranch, selectDefaultBranch, selectTruthyItems } from '../../shared/selectors';
 import { green, red, yellow } from '../../shared/colors';
-import getBranchDetails, { BranchDetails } from '../../shared/getBranchDetails';
+import gitLogOneLine from '../../shared/gitLogOneLine';
 
-const errorMessage = `FAILED to switch branches`
+const errorMessage = `FAILED to rebase commits`
 
-const NormalizeBranchNames = (defaultBranch: string, currentBranch: string) =>
-  (collection: string[], { name, isStale, lastCommitDate }: BranchDetails ) => {
-    const isCurrentBranch = currentBranch === name
-    if(isCurrentBranch) {
-      return collection
-    }
-
-    const defaultTag = defaultBranch === name ? green('(default)') : ''
-    const staleTag = isStale ? red('(stale)') : yellow('(not-stale)').dim
-    const lastUpdated = yellow(`last-updated: ${lastCommitDate}`).dim
-
-    return [
-      ...collection,
-      `${name} ${defaultTag} ${staleTag} ${lastUpdated}`
-    ]
-  }
-
-async function selectBranch() {
-  const defaultBranch = await selectDefaultBranch()
-  const currentBranch = await selectCurrentBranch()
-
-  const branchNames = (await getBranchDetails(false))
-    .reduce(NormalizeBranchNames(defaultBranch, currentBranch), [] as string[])
-
-  const answers = await inquirer.prompt([
-    {
-      type: 'list',
-      name: 'branch',
-      message: 'Choose your destination, my love.',
-      choices: branchNames
-    }
-  ]);
-
-  return answers.branch.split(' ')[0]
+const rebaseCommits = async () => {
+  const selectLogs = true
+  const commitTag = await gitLogOneLine(selectLogs)
+  const rebaseCommand = `git rebase -i ${commitTag}`
+  echo(yellow(rebaseCommand))
+  exec(rebaseCommand, {async: true})
+  echo(green('Your rebasing now!'))
 }
 
-async function switchBranch(branchName: string) {
-  echo(yellow(`git checkout ${branchName}`))
-  await exec(`git checkout ${branchName}`)
-  echo(green("Switched"))
-  exit(0)
-}
-
-const checkOut = async () => {
-  config.silent = true
-
-  let branchName = process.argv[2]
-  if(branchName) {
-    await switchBranch(branchName)
-  }
-
-  branchName = await selectBranch()
-  await switchBranch(branchName)
-}
-
-(async () => await errorHandlerWrapper(checkOut, errorMessage))();
+(async () => await errorHandlerWrapper(rebaseCommits, errorMessage))();
