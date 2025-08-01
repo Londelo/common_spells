a2df8458adfe408347f312456524535c8d4fb115
TM_Brodie <brodie.balser@ticketmaster.com>
2025-08-01 16:17:05 -0400

    RELEASE: prep

--- src/spells/tm/gitLabRelease.ts
+++ src/spells/tm/gitLabRelease.ts
@@ -6,11 +6,17 @@
 //check to see if we are logged in with glab auth status
 // if not then tell the user to run glab auth login --hostname git.tmaws.io --token [access token]
 
+// use the fetchAndPull function in src/shared/fetchAndPull
+
 //get the latest release version with glab release view
 
 //get the default branch useing the selectDefaultBranch in src/shared/selectors
 
 // execute git shortlog --no-merges [latestRelease]..[defaultBranch]
 // store this output in a variable called releaseNotes
+
+// prompt for a new version
+
+// prompt to confirm next step display this glab release create [latestVersion] --notes "```\n[releaseNotes]\n```"
 
 // execute glab release create [latestVersion] --notes "```\n[releaseNotes]\n```"
