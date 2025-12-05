adb654a9ab69dbdb621ce31d09d2844e9e0c1d56
TM_Brodie <brodie.balser@ticketmaster.com>
2025-12-05 09:31:13 -0500

    FIXUP-1: updated install

--- src/spells/install.ts
+++ src/spells/install.ts
@@ -3,14 +3,15 @@ import { exec, echo } from 'shelljs'
 import errorHandlerWrapper from '../shared/errorHandlerWrapper'
 import { green, yellow } from '../shared/colors'
 import { selectAllArgs } from '../shared/selectors'
+import { execute } from '../shared/shell'
 
 const errorMessage = 'FAILED to install'
 const findNpmLockFile = 'find . -path ./package-lock.json'
 const findYarnLockFile = 'find . -path ./yarn.lock'
 
 const install = async () => {
-  const npmLockFile = exec(findNpmLockFile, {silent: true}).stdout
-  const yarnLockFile = exec(findYarnLockFile, {silent: true}).stdout
+  const npmLockFile = await execute(findNpmLockFile, 'Failed to find lock file for npm')
+  const yarnLockFile = await execute(findYarnLockFile, 'Failed to find lock file for yarn')
   const installParams = selectAllArgs() || ''
   let installCommand = ''
 
@@ -26,7 +27,7 @@ const install = async () => {
   }
 
   echo(yellow(installCommand))
-  exec(installCommand)
+  await execute(installCommand, `Failed to run: ${installCommand}`)
   echo(green('Install Complete!'))
 }
 
