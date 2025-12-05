6918a111e58aa660fa65f882832608065a7e8103
TM_Brodie <brodie.balser@ticketmaster.com>
2025-12-05 09:45:14 -0500

    FIXUP-1: updated force push

--- src/spells/git/forcePush.ts
+++ src/spells/git/forcePush.ts
@@ -1,13 +1,28 @@
 #!/usr/bin/env node
-import { exec, echo } from 'shelljs'
+import { echo } from 'shelljs'
 import errorHandlerWrapper from '../../shared/errorHandlerWrapper'
-import { green } from '../../shared/colors'
+import { green, yellow } from '../../shared/colors'
+import { execute } from '../../shared/shell'
+import { selectAllArgs } from '../../shared/selectors'
 
 const errorMessage = 'you are not a true dragon born.'
+const dangerousPush = '--dangerous'
 
 const forcePush = async () => {
   echo(green('FUS-RO-DAAAAAHHH!'))
-  exec('git push --force-with-lease --no-verify')
+
+  const allArguments = selectAllArgs()
+  const dangerouslyPush = allArguments.includes(dangerousPush)
+
+  if(dangerouslyPush) {
+    const command = 'git push --force --no-verify'
+    echo(yellow(command))
+    await execute(command, 'Failed to dangerously force push')
+  } else {
+    const command = 'git push --force-with-lease --no-verify'
+    echo(yellow(command))
+    await execute(command, 'Failed to safely force push')
+  }
 }
 
 (async () => await errorHandlerWrapper(forcePush, errorMessage))();
